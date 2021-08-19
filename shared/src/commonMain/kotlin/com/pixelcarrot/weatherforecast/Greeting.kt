57e6db441c6a1f128324df02c8d81481c4aadfbc
Justin Nguyen <justinnguyen@outlook.sg>
2021-08-19 23:48:36 +0700

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/pixelcarrot/weatherforecast/Greeting.kt
@@ -0,0 +1,7 @@
+package com.pixelcarrot.weatherforecast
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/pixelcarrot/weatherforecast/Platform.kt
@@ -0,0 +1,5 @@
+package com.pixelcarrot.weatherforecast
+
+expect class Platform() {
+    val platform: String
+}
