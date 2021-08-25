1e986ac698fcfd1c70b3d5fde6e5ea6bc922d011
Justin Nguyen <justinnguyen@outlook.sg>
2021-08-25 15:31:58 +0700

    Update unsplash mock

--- iosApp/iosApp/Home/HomeView.swift
+++ iosApp/iosApp/Home/HomeView.swift
@@ -44,13 +44,13 @@ struct HomeView: View {
 
 struct HomeView_Previews: PreviewProvider {
     static var viewModel = HomeViewModel()
-    static var background = "https://images.unsplash.com/photo-1561074557-5a1c929e7535?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+    static var background = "https://images.unsplash.com/photo-1629875900852-8ea865ec48f5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
     
     static var previews: some View {
         Group {
             HomeView(viewModel: viewModel)
                 .onAppear(perform: {
-                    viewModel.state = .loaded(weather: Weather(city: "City", country: "Country", temperature: 30.0, feelsLike: 28.0, condition: "Broken Clouds", icon: ""), image: WeatherImage(imageUrl: background, author: "Justin Nguyen", authorUrl: "https://pixelcarrot.com/"))
+                    viewModel.state = .loaded(weather: Weather(city: "City", country: "Country", temperature: 30.0, feelsLike: 28.0, condition: "Broken Clouds", icon: ""), image: WeatherImage(imageUrl: background, author: "Justin Nguyen", authorUrl: "https://unsplash.com/@pixelcarrot"))
                 })
         }
     }
